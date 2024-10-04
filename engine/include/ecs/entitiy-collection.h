582c21ba41fe7f3432dfe5a577d2118d67aeca1a
10Narratives <vanyatertyshnikov@yandex.ru>
2024-10-04 21:29:56 +0300

    Replace EntityRelations on EntityNode
    EntityNode presents a node in an entity tree.
    It handles ComponentCollection and relations between it and another
    nodes.

--- engine/include/ecs/entitiy-collection.h
+++ engine/include/ecs/entitiy-collection.h
@@ -1,7 +1,3 @@
 #pragma once
 
-namespace engine::ecs {
-// Insertion of simple entity (just a one tree node)
-// Insertion of complex entity (a subtree)
-
-}
+namespace engine::ecs {}  // namespace engine::ecs
